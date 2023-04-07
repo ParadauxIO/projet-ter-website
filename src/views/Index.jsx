6861e6b0d3db74c475e55a6ed59ba4d25e6f4dcf
Rían Errity <rian@paradaux.io>
2023-04-07 15:21:45 +0200

    Move index over to new layout, finish sidebar for now

--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -1,41 +1,12 @@
-import { useEffect, useState } from "react"
-import Navbar from "../components/Navbar"
+
 import "./Index.scss"
-import { getMainTable } from "../partials/italyDataHandler"
-import { useTable } from "react-table";
-import TerTable from "../components/TerTable";
+
 import Footer from "../components/Footer";
 import Sidebar from "../components/Sidebar";
 import { Outlet } from "react-router-dom";
 
 export default function Index() {
-    let [tableDataRaw, setTableDataRaw] = useState([]);
-    let [columnData, setColumnData] = useState([]);
 
-    const tableInstance = useTable({ columns: columnData, data: tableDataRaw })
-
-    useEffect(() => {
-        async function load() {
-            let rawTableData = await getMainTable();
-            setTableDataRaw(rawTableData);
-
-            let columns = [];
-            for (let row of rawTableData) {
-                for (let key of Object.keys(row)) {
-                    if (!doesContainNestedKeyValue(columns, 'Header', key)) {
-                        columns.push({
-                            Header: key,
-                            accessor: key
-                        })
-                    }
-                }
-            }
-            console.log(tableDataRaw)
-            setColumnData(columns);
-        }
-
-        load();
-    }, [])
 
     return (
         <div class="grid-container">
@@ -47,10 +18,3 @@ export default function Index() {
     )
 }
 
-let doesContainNestedKeyValue = (array, key, value) => {
-    for (let v of array) {
-        if (v[key] && v[key] == value) return true;
-    }
-
-    return false;
-}

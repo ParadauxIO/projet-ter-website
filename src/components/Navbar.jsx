578ca79d95fc3938463e466674b68e876289ce19
Rían Errity <rian@paradaux.io>
2023-04-07 12:43:36 +0200

    Slightly nicer nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,10 @@ export default function Navbar() {
     return (
         <nav className="desktop-nav">
             <span className="logo">Projet TER Website</span>
-            <a href="/">Main Table</a>
-            <a href="/coordinates">Coordinates</a>
+            <div className="nav-links">
+                <a href="/">Main Table</a>
+                <a href="/coordinates">Coordinates</a>
+            </div>
         </nav>
     )
 }

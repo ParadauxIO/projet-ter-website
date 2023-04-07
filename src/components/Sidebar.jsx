9687992a577dcfcd9c5c2aa1a39fb278e33a9fab
Rían Errity <rian@paradaux.io>
2023-04-07 15:21:55 +0200

    Finsih sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,17 @@
+import "./Sidebar.scss"
+
 export default function Sidebar() {
     return (
         <aside className="sidenav">
-            <div className="header">
+            <div className="header-side">
+                <h1>Projet D'Integration</h1>
+                <p>Web Interface v1.0</p>
+            </div>
 
+            <div className="links">
+                <a className="link active"> Home </a>
+                <a className="link"> Coordinate Table </a>
+                More to come!
             </div>
         </aside>
     )

797841a72cf53fb76ab7670fe0a223fa1021f7e0
Rían Errity <rian@paradaux.io>
2023-04-07 15:27:44 +0200

    Fix footer font and style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
+import "./Footer.scss"
+
 export default function Footer() {
     return (
         <footer className="footer">
             <p>
                 Source code for the User Interface available <a href="https://github.com/paradauxIO/projet-ter-website">here</a>.
-                More pages to come featuring data from other tables more specifically
-                Still working on data entry.
+                More pages to come featuring data from other tables more specifically.
+                Designed by <a href="https://clove.ie">Clove.ie</a>
             </p>
         </footer>
     )

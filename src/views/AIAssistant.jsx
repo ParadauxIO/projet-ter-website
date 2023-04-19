b400629f48e6e165d18384af24ef37784130739c
Rían Errity <rian@paradaux.io>
2023-04-19 14:04:38 +0200

    Begin doing up the AI assistant page

--- src/views/AIAssistant.jsx
+++ src/views/AIAssistant.jsx
@@ -1,11 +1,26 @@
 import "./MainGrid.scss"
+import "./AIAssistant.scss"
 
 export default function AIAssistant() {
     return (
         <main className="main">
-            <p>
-                Coming soon!
-            </p>
+            <div className="ai-assistant">
+                <div className="card">
+                    <h1>
+                        AI Assistant
+                    </h1>
+                    <p>
+                        Powered by GPT-3.5-Turbo/ChatGPT ask an AI assistant questions
+                        about the data contained within the database and receive natural
+                        language feedback and responses!
+                    </p>
+
+                    <form>
+                        <textarea className="card"/>
+                        <input type="submit" value="submit"/>
+                    </form>
+                </div>
+            </div>
         </main>
     )
 }

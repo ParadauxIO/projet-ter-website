2cbb63276f9e2a50b0153d3329a098786306f798
Rían Errity <rian@paradaux.io>
2023-04-19 13:15:29 +0200

    More dynamic routing

--- src/main.jsx
+++ src/main.jsx
@@ -8,34 +8,8 @@ import { RecoilRoot } from 'recoil'
 import Index from './views/Index';
 import "./global.scss"
 import Home from './views/Home';
-import AllColumnsJoined from './views/tables/AllColumnsJoined';
 import DerivedTableView from './views/tables/DerivedTableView';
-import { dbCoordinateColumnsState } from './state/atoms/dbDataAtom';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Index />,
-    children: [{
-      index: true,
-      element: <Home/>
-    },
-    {
-      path: "all",
-      element: <AllColumnsJoined />
-    },
-    {
-      path: "coordinates",
-      element: <DerivedTableView 
-                 title="Coordinates Data"
-                 subtitle="Shows co-ordinate data."
-                 columnsState={dbCoordinateColumnsState}
-               />
-    }
-    ]
-  },
-
-]);
+import { dbAclsAtlasColumnsState, dbAclsMapColumnsState, dbCaAtlasColumnsState, dbColumnsState, dbCoordinateColumnsState, dbHarvardLongListColumnsState, dbLpmColumnsState, dbSampleRootColumnsState, dbZoneColumnsState } from './state/atoms/dbDataAtom';
 
 function App() {
   return (
@@ -49,3 +23,80 @@ function App() {
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+
+
+export const tableRoutes = [
+  {
+    path: "sample-coordinates",
+    title: "Sample Coordinate Data",
+    subtitle: "Details about a sample's coordinates",
+    state: dbCoordinateColumnsState
+  },
+  {
+    path: "acls-atlas",
+    title: "ACLS Atlas Data",
+    subtitle: "Atlas details from ACLS",
+    state: dbAclsAtlasColumnsState
+  },
+  {
+    path: "acls-map",
+    title: "ACLS Map Data",
+    subtitle: "Map details from ACLS",
+    state: dbAclsMapColumnsState
+  },
+  {
+    path: "ca-atlas",
+    title: "CA Atlas",
+    subtitle: "Atlas details from CA",
+    state: dbCaAtlasColumnsState
+  },
+  {
+    path: "harvard-long-list",
+    title: "Harvard Long List Data",
+    subtitle: "Data from Harvard",
+    state: dbHarvardLongListColumnsState
+  },
+  {
+    path: "lpm-data",
+    title: "LPM Data",
+    subtitle: "Data from LPM",
+    state: dbLpmColumnsState
+  },
+  {
+    path: "sample-root",
+    title: "Sample Root Data",
+    subtitle: "Data about the sample root.",
+    state: dbSampleRootColumnsState
+  },
+  {
+    path: "zone",
+    title: "Italy Zone Data",
+    subtitle: "Zone informations",
+    state: dbZoneColumnsState
+  },
+];
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />,
+    children: [{
+      index: true,
+      element: <Home/>
+    },
+    {
+      path: "/table",
+      children: tableRoutes.map(tableRoute => ({
+        path: tableRoute.path,
+        element: (
+          <DerivedTableView
+            title={tableRoute.title}
+            subtitle={tableRoute.subtitle}
+            columnsState={tableRoute.columnsState}
+          />
+        )
+      }))
+    }
+  ]
+},
+]);
--- src/state/atoms/dbDataAtom.js
+++ src/state/atoms/dbDataAtom.js
@@ -1,5 +1,14 @@
 import { atom, selector } from "recoil";
 
+const coordinateColumns = ["name", "coordinates_ge_osm", "s_centre", "coordinates_polygon_ge", "coordinates_centre_georectified", "archives_list", "archives_list_text"];
+const aclsAtlasColumns = [];
+const aclsMapColumns = [];
+const caAtlasColumns = [];
+const harvardLongListColumns = [];
+const lpmColumns = [];
+const sampleRootColumns = [];
+const zoneColumns = [];
+
 export const dbColumnsState = atom({
     key: "dbColumnsState",
     default: [],
@@ -14,7 +23,70 @@ export const dbCoordinateColumnsState = selector({
     key: 'dbCoordinateColumnsState',
     get: ({get}) => {
       return get(dbColumnsState).filter(item =>
-        ["name", "coordinates_ge_osm", "s_centre", "coordinates_polygon_ge", "coordinates_centre_georectified", "archives_list", "archives_list_text"].includes(item.accessor)
+        coordinateColumns.includes(item.accessor)
       )
     },
-  });
+});
+
+export const dbAclsAtlasColumnsState = selector({
+  key: "dbAclsAtlasColumnsState",
+  get: ({get}) => {
+    return get(dbColumnsState).filter(item =>
+      aclsAtlasColumns.includes(item.accessor)
+    )
+  }
+});
+
+export const dbAclsMapColumnsState = selector({
+  key: "dbAclsMapColumnsState",
+  get: ({get}) => {
+    return get(dbColumnsState).filter(item =>
+      aclsMapColumns.includes(item.accessor)
+    )
+  }
+});
+
+export const dbCaAtlasColumnsState = selector({
+  key: "dbCaAtlasColumnsState",
+  get: ({get}) => {
+    return get(dbColumnsState).filter(item =>
+      caAtlasColumns.includes(item.accessor)
+    )
+  }
+});
+
+export const dbHarvardLongListColumnsState = selector({
+  key: "dbHarvardLongListColumnsState",
+  get: ({get}) => {
+    return get(dbColumnsState).filter(item =>
+      harvardLongListColumns.includes(item.accessor)
+    )
+  }
+});
+
+export const dbLpmColumnsState = selector({
+  key: "dbLpmColumnsState",
+  get: ({get}) => {
+    return get(dbColumnsState).filter(item =>
+      lpmColumns.includes(item.accessor)
+    )
+  }
+});
+
+export const dbSampleRootColumnsState = selector({
+  key: "dbSampleRootColumnsState",
+  get: ({get}) => {
+    return get(dbColumnsState).filter(item =>
+      sampleRootColumns.includes(item.accessor)
+    )
+  }
+});
+
+export const dbZoneColumnsState = selector({
+  key: "dbZoneColumnsState",
+  get: ({get}) => {
+    return get(dbColumnsState).filter(item =>
+      zoneColumns.includes(item.accessor)
+    )
+  }
+});

10a2f1aa3faf53fb8514802020a0c2d5089edbc5
Rían Errity <rian@paradaux.io>
2023-04-25 10:59:45 +0200

    Add todo note

--- src/views/MapView.jsx
+++ src/views/MapView.jsx
@@ -3,16 +3,21 @@ import "./MapView.scss"
 import "./HomeDashboard.scss"
 
 export default function MapView() {
+    const mapCenter = [42.2619188, 13.7007411];
+    const zoom = 6;
+
+
     return (
         <div className="map-view">
             <div className='map-header'>
                 <h1>Maps</h1>
+                <p> TODO: add options and pins to the map</p>
             </div>
             <div className="center-map">
-                <MapContainer center={[42.2619188,13.7007411]} zoom={6} scrollWheelZoom={false}>
+                <MapContainer center={mapCenter} zoom={zoom} scrollWheelZoom={true}>
                     <TileLayer
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors and Rían Errity .............'
                     />
                 </MapContainer>
             </div>

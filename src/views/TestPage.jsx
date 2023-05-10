7f500bb58abbdc711791296a6e53317b95c8976e
Rían Errity <rian@paradaux.io>
2023-05-10 14:58:05 +0200

    Update testpage

--- src/views/TestPage.jsx
+++ src/views/TestPage.jsx
@@ -1,63 +1,44 @@
-import React, { useState, useRef, useEffect, useMemo, useCallback} from 'react';
-import { createRoot } from 'react-dom/client';
-import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
+import { useState,useEffect} from 'react';
 
-import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
-import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
+import cross from '../assets/cross.svg'
+import tick from '../assets/tick.svg'
+import "./Test.scss"
 
 const TestPage = () => {
 
- const gridRef = useRef(); // Optional - for accessing Grid's API
- const [rowData, setRowData] = useState(); // Set rowData to Array of Objects, one Object per Row
+  let [values, setValues] = useState([true, true, true, true, true, true, true, true, true, true])
+  let labels = [
+    "brocarts",
+    "rorcarts",
+    "borcarts",
+    "broarcts",
+    "broctsca",
+    "bortarts",
+    "borortss",
+    "borrcart",
+    "orbartsb",
+    "brocarts"
+  ]
 
- // Each Column Definition results in one Column.
- const [columnDefs, setColumnDefs] = useState([
-   {field: 'make', filter: true},
-   {field: 'model', filter: true},
-   {field: 'price'}
- ]);
-
- // DefaultColDef sets props common to all Columns
- const defaultColDef = useMemo( ()=> ({
-     sortable: true
-   }));
-
- // Example of consuming Grid Event
- const cellClickedListener = useCallback( event => {
-   console.log('cellClicked', event);
- }, []);
-
- // Example load data from server
- useEffect(() => {
-   fetch('https://www.ag-grid.com/example-assets/row-data.json')
-   .then(result => result.json())
-   .then(rowData => setRowData(rowData))
- }, []);
-
- // Example using Grid's API
- const buttonListener = useCallback( e => {
-   gridRef.current.api.deselectAll();
- }, []);
+  useEffect(() => {
+    let newValues = [...values];
+    for (let i = 0; i < 3; i++) {
+      newValues[Math.floor(Math.random() * 9) + 0] = false;
+    }
+    setValues(newValues);
+  }, [])
 
  return (
-   <div>
-     {/* Example using Grid's API */}
-     <button onClick={buttonListener}>Push Me</button>
-
-     {/* On div wrapping Grid a) specify theme CSS Class Class and b) sets Grid size */}
-     <div className="ag-theme-alpine" style={{width: 500, height: 500}}>
-
-       <AgGridReact 
-           ref={gridRef} // Ref for accessing Grid's API 
-           rowData={rowData} // Row Data for Rows 
-           columnDefs={columnDefs} // Column Defs for Columns 
-           defaultColDef={defaultColDef} // Default Column Properties 
-           animateRows={true} // Optional - set to 'true' to have rows animate when sorted
-           rowSelection='multiple' // Options - allows click selection of rows 
-           onCellClicked={cellClickedListener} // Optional - registering for Grid Event 
-           />
-        </div>
-   </div>
+    <div class="test">
+      <ul>
+        {
+          values.map((value, index) => <li>
+            <img src={value ? tick : cross} width="32px"/>
+            &nbsp; {labels[index]}
+          </li>)
+        }
+      </ul>
+    </div>
  );
 };
 
